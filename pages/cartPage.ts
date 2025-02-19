3863b6be041a419f88ec9edf99039ba21e7ed827
Ervin Nisperos <jbnisperos07@gmail.com>
2025-02-19 16:18:59 +1100

    added logs
    added logs

--- pages/cartPage.ts
+++ pages/cartPage.ts
@@ -1,21 +1,26 @@
-import { Locator, Page } from '@playwright/test';
+import { Locator, Page } from "@playwright/test";
+import logger from "../helper/logger";
 
 export class CartPage {
-    readonly page: Page;
-    readonly checkoutButton: Locator;
-    readonly itemQuantity: Locator;
+  readonly page: Page;
+  readonly checkoutButton: Locator;
+  readonly itemQuantity: Locator;
 
-    constructor(page: Page) {
-        this.page = page
-        this.checkoutButton = this.page.locator('[data-test="checkout"]');
-        this.itemQuantity = this.page.locator('[data-test="item-quantity"]');
-    }
+  constructor(page: Page) {
+    this.page = page;
+    this.checkoutButton = this.page.locator('[data-test="checkout"]');
+    this.itemQuantity = this.page.locator('[data-test="item-quantity"]');
+  }
 
-    async proceedToCheckout() {
-        await this.checkoutButton.click();
-    }
+  async proceedToCheckout() {
+    logger.info("Proceeding to checkout...");
+    await this.checkoutButton.click();
+    logger.info("Clicked 'Checkout' button.");
+  }
 
-    async getItemQuantity(): Promise<string> {
-    return await this.itemQuantity.innerText();
+  async getItemQuantity(): Promise<string> {
+    const quantity = await this.itemQuantity.innerText();
+    logger.info(`Cart item quantity: ${quantity}`);
+    return quantity;
   }
 }
--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -1,4 +1,5 @@
 import { Locator, Page } from '@playwright/test';
+import logger from "../helper/logger"; 
 
 export class LoginPage {
     readonly page: Page;
@@ -14,9 +15,11 @@ export class LoginPage {
     }
 
     async login(username: string, password: string) {
+        logger.info(`Attempting login with username: ${username}`);
         await this.usernameField.fill(username); 
         await this.passwordField.fill(password);
-        await this.loginButton.click();     
+        await this.loginButton.click();
+        logger.info("Login button clicked.");
     }
 
     async navigate(url: string) {
